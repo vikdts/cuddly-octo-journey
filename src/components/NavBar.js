bd9bb735e422249f58c552a1a6ed2c76d54f6246
vikdts <v.d.tsareva@gmail.com>
2025-01-22 11:13:04 +0200

    push links to the right

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,7 +9,7 @@ const NavBar = () => {
         <Navbar.Brand><img src={logo} alt="logo" height="45"/></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
+          <Nav className="ml-auto">
             <Nav.Link><i className="far fa-circle"></i>Home</Nav.Link>
             <Nav.Link><i className="far fa-arrow-alt-circle-right"></i>Sign in</Nav.Link>
             <Nav.Link><i className="fas fa-user-circle"></i>Sign up</Nav.Link>
