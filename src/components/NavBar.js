49d7c7ccabeef0f95f3c67bf5c0234f76d8477c0
vikdts <v.d.tsareva@gmail.com>
2025-01-22 10:33:46 +0200

    import bootstap react navbar boiler plate

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Link</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
   )
 }
 
