c3833429c10a83b456a36205df889ac97617bc08
vikdts <v.d.tsareva@gmail.com>
2025-01-24 14:29:28 +0200

    wrap heading and link in column

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -5,12 +5,14 @@ import { Col, Row, Container } from "react-bootstrap";
 const SignUpForm = () => {
   return (
     <Row>
+        <Col>
       <Container>
         <h1>sign up</h1>
       </Container>
       <Container>
         <Link>Have an account?</Link>
       </Container>
+      </Col>
     </Row>
   );
 };
