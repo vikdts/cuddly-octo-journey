9559500af9fc3f334a8c45a8a53f76b79d303878
vikdts <v.d.tsareva@gmail.com>
2025-01-27 11:10:07 +0200

    update input type to text

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -14,21 +14,20 @@ const SignUpForm = () => {
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>sign up</h1>
           <Form>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
- 
-  </Form.Group>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="text" placeholder="Enter email" />
+            </Form.Group>
 
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
 
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
           <Link className={styles.Link} to="/signin">
