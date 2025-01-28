1b7c5fb79968fba043f34d245359c6ef015cdbb8
vikdts <v.d.tsareva@gmail.com>
2025-01-28 18:07:06 +0200

    import django user to pprofile models

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../assets/logo.png";
+import styles from "../styles/NavBar.module.css"
+import { NavLink} from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
+      <Container>
+        <NavLink to="/">
+        <Navbar.Brand><img src={logo} alt="logo" height="45"/></Navbar.Brand>
+        </NavLink>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/"><i className="far fa-circle"></i>Home</NavLink>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/sigin"><i className="far fa-arrow-alt-circle-right"></i>Sign in</NavLink>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup"><i className="fas fa-user-circle"></i>Sign up</NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
