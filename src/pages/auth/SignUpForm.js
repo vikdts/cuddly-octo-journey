b2b7e976efd96dfd0aea0c7cbcf3e5ba0a9e9e87
vikdts <v.d.tsareva@gmail.com>
2025-01-28 10:59:42 +0200

    add alerts for non field errors

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -111,6 +111,11 @@ const SignUpForm = () => {
             >
               Sign up
             </Button>
+            {errors.non_field_errors?.map((message, idx) => (
+              <Alert key={idx} variant="warning" className="mt-3">
+                {message}
+              </Alert>
+            ))}
           </Form>
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
