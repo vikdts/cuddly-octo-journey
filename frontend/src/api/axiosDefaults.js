a2dc4a7afc95e26521be38a3ae2be7a220e6e405
vikdts <v.d.tsareva@gmail.com>
2025-01-28 13:41:39 +0200

    add new django project

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://cuddly-octo-journey-9a4868450ef0.herokuapp.com/';
+axios.defaults.baseURL = 'https://cuddly-octo-journey-9a4868450ef0.herokuapp.com';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
