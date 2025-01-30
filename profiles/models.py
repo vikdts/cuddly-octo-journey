d1658d303bbd0e5acdea13c6207f0d370a2c6669
vikdts <v.d.tsareva@gmail.com>
2025-01-30 11:51:23 +0200

    add updated_at timestamp

--- profiles/models.py
+++ profiles/models.py
@@ -5,3 +5,5 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
