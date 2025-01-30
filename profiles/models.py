b4d3846f61c2119b59ffd2ff61f1ac1c744ee4ec
vikdts <v.d.tsareva@gmail.com>
2025-01-30 13:01:26 +0200

    add img field with upload_to param

--- profiles/models.py
+++ profiles/models.py
@@ -8,4 +8,6 @@ class Profile(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
-
+    image = models.ImageField(
+        upload_to='images/', default='default_profile_reesea'
+    )
