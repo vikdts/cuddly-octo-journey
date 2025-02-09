be4dcffbc920d3debacf72547346ae0e1770d590
vikdts <v.d.tsareva@gmail.com>
2025-02-09 20:19:56 +0200

    spcify fields to include in the response

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -3,6 +3,10 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    
+
     class Meta:
         model = Profile
+        fields = [
+            'id', 'owner', 'created_at', 'updated_at', 'name',
+            'content', 'image'
+        ]
