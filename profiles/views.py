af3d7043adf056742b358fe1877aa86066b3d62b
vikdts <v.d.tsareva@gmail.com>
2025-02-09 19:42:03 +0200

    import ProfileSerializer

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
+from .serializers import ProfileSerializer
 
 # Create your views here.
 class ProfileList(APIView):
     def get(self, request):
     profiles = Profile.objects.all()
-
+    return Response(serializer.data)
