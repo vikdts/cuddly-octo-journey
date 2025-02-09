339e3aa6d1114075b1bca838fa3e1dda2aa57470
vikdts <v.d.tsareva@gmail.com>
2025-02-09 14:04:22 +0200

    update default img file path

--- profiles/models.py
+++ profiles/models.py
@@ -9,5 +9,5 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='default_profile_reesea'
+        upload_to='images/', default='../default_profile_reesea'
     )
