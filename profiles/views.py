b441cf53e0fe1cd2b81f2b94f0cb64a9a1c7f86c
vikdts <v.d.tsareva@gmail.com>
2025-02-12 14:38:49 +0200

    pass request to put method

--- profiles/views.py
+++ profiles/views.py
@@ -35,7 +35,9 @@ class ProfileDetail(APIView):
 
     def put(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data)
+        serializer = ProfileSerializer(
+            profile, data=request.data, context={'request': request}
+        )
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
