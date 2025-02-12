ac6ff7a2be7b3bf13bb0a74904c75c189b3f551e
vikdts <v.d.tsareva@gmail.com>
2025-02-12 13:09:31 +0200

    if serilizer is valid, save profile instance to the db

--- profiles/views.py
+++ profiles/views.py
@@ -27,4 +27,7 @@ class ProfileDetail(APIView):
     def put(self, request, pk):
         profile = self.get_object(pk)
         serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+        serializer.save()
+
 
