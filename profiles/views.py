fef6512a4b29accff86ecacfa1a970e835de07b4
vikdts <v.d.tsareva@gmail.com>
2025-02-12 14:05:44 +0200

    import custom permission to ProfileDetail view

--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
+from drf_api.permissions import IsOwnerOrReadOnly
 
 # Create your views here.
 class ProfileList(APIView):
