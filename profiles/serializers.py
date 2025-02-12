53820c4c0dc15ae5d1a6ab78fb732489765fac4a
vikdts <v.d.tsareva@gmail.com>
2025-02-12 14:40:58 +0200

    accesss context object inside get_is_owner method

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -6,6 +6,7 @@ class ProfileSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
+        request = self.context['request']
 
     class Meta:
         model = Profile
