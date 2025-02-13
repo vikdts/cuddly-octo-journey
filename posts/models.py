71a249ef345fb21644cc35135f61dff471150aa1
vikdts <v.d.tsareva@gmail.com>
2025-02-13 14:43:32 +0200

    define a list for the image_filter in Post model

--- posts/models.py
+++ posts/models.py
@@ -3,6 +3,15 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
+    image_filter_choices = [
+    ('_1977', '1977'), ('brannan', 'Brannan'),
+    ('earlybird', 'Earlybird'), ('hudson', 'Hudson'),
+    ('inkwell', 'Inkwell'), ('lofi', 'Lo-Fi'),
+    ('kelvin', 'Kelvin'), ('normal', 'Normal'),
+    ('nashville', 'Nashville'), ('rise', 'Rise'),
+    ('toaster', 'Toaster'), ('valencia', 'Valencia'),
+    ('walden', 'Walden'), ('xpro2', 'X-pro II')
+    ]
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -13,7 +22,7 @@ class Post(models.Model):
     )
     class Meta:
         ordering = ['-created_at']
-        
+
     def __str__(self):
         return f'{self.id} {self.title}'
 
