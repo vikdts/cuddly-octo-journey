4a0fe15d102f8aa9a26f872928257873ea32d031
vikdts <v.d.tsareva@gmail.com>
2025-02-13 16:21:17 +0200

    retrieve PostList View

--- posts/views.py
+++ posts/views.py
@@ -8,7 +8,34 @@ from .serializers import PostSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
 
 # Create your views here.
+
 class PostList(APIView):
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+    serializer_class = PostSerializer
+
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(
+            posts, many=True, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PostSerializer(
+            data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+class PostDetail(APIView):
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly
     ]
