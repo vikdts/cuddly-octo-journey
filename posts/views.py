8a770287cd92968b4f97d11821e13f018f4cf8de
vikdts <v.d.tsareva@gmail.com>
2025-02-13 15:40:11 +0200

    set permission_classes to IsAuthenticatedOrReadOnly

--- posts/views.py
+++ posts/views.py
@@ -8,6 +8,9 @@ from .serializers import PostSerializer
 # Create your views here.
 class PostList(APIView):
     serializer_class = PostSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
 
     def get(self, request):
         posts = Post.objects.all()
