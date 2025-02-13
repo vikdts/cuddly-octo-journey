96dba7903360d29ab2f938f407f6ea4ded1333f8
vikdts <v.d.tsareva@gmail.com>
2025-02-13 13:27:11 +0200

    populate profile_image with owners profilee img url

--- posts/serializers.py
+++ posts/serializers.py
@@ -6,3 +6,4 @@ class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
