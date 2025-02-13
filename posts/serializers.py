d1a4f1703c921e83dc22ce78bd5ae6e738fbea97
vikdts <v.d.tsareva@gmail.com>
2025-02-13 13:47:26 +0200

    link Meta class to Post model

--- posts/serializers.py
+++ posts/serializers.py
@@ -11,3 +11,6 @@ class PostSerializer(serializers.ModelSerializer):
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    class Meta:
+        model = Post
