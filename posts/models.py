a615f406326f2ace63a9b196c939d1a222308a47
vikdts <v.d.tsareva@gmail.com>
2025-02-13 12:40:17 +0200

    add created_at and updated_at timestamps

--- posts/models.py
+++ posts/models.py
@@ -4,3 +4,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
