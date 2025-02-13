1639335c92950e072f0e373b7c065cc9869b4ba0
vikdts <v.d.tsareva@gmail.com>
2025-02-13 12:41:58 +0200

    add content Charfield

--- posts/models.py
+++ posts/models.py
@@ -7,5 +7,6 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
 
 
