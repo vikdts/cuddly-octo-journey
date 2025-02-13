a02ec50d66c1469bd9c7de7dededf60b677bf4dd
vikdts <v.d.tsareva@gmail.com>
2025-02-13 14:53:53 +0200

    raise error if height exceeds 4096px

--- posts/serializers.py
+++ posts/serializers.py
@@ -11,6 +11,10 @@ class PostSerializer(serializers.ModelSerializer):
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
             raise serializers.ValidationError('Image size larger than 2MB!')
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px!'
+            )
 
     def get_is_owner(self, obj):
         request = self.context['request']
