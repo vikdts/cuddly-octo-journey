60e0ac3892dafc6707fc6ae34574dad8034d7f04
vikdts <v.d.tsareva@gmail.com>
2025-02-13 13:39:15 +0200

    define get_is_owner method

--- posts/serializers.py
+++ posts/serializers.py
@@ -7,3 +7,5 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def get_is_owner(self, obj):
