e9c0c6cb76a1827434474ef382f875230a50e024
vikdts <v.d.tsareva@gmail.com>
2025-02-13 15:04:05 +0200

    import PostSerializer

--- posts/views.py
+++ posts/views.py
@@ -3,5 +3,6 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Post
+from .serializers import PostSerializer
 
 # Create your views here.
