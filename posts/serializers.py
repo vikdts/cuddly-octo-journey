7b06bb48314f6c8545db27c25b4185e29962990e
vikdts <v.d.tsareva@gmail.com>
2025-02-13 13:48:41 +0200

    specify felds to include in the response

--- posts/serializers.py
+++ posts/serializers.py
@@ -14,3 +14,8 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id',
+            'profile_image', 'created_at', 'updated_at',
+            'title', 'content', 'image'
+        ]
