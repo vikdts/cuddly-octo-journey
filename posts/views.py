da455cbd98b93024a9175f2707f450c9517225e0
vikdts <v.d.tsareva@gmail.com>
2025-02-13 15:35:18 +0200

    else return serializer errors with 400

--- posts/views.py
+++ posts/views.py
@@ -23,3 +23,6 @@ class PostList(APIView):
             return Response(
                 serializer.data, status=status.HTTP_201_CREATED
             )
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
