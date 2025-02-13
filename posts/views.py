466ec8168eb40c9caf170921d3d27ddf16b93475
vikdts <v.d.tsareva@gmail.com>
2025-02-13 15:03:43 +0200

    import Post model to views

--- posts/views.py
+++ posts/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .models import Post
 
 # Create your views here.
