96766749e13da7252026c0788794c5a10315cba6
vikdts <v.d.tsareva@gmail.com>
2025-02-14 13:58:04 +0200

    assert response to status 201

--- profiles/tests.py
+++ profiles/tests.py
@@ -21,3 +21,4 @@ class PostListViewTests(APITestCase):
         response = self.client.post('/posts/', {'title': 'a title'})
         count = Post.objects.count()
         self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
