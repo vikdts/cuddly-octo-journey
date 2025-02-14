98d6b1e636cc1c444346a5bb210772b217f58b1a
vikdts <v.d.tsareva@gmail.com>
2025-02-14 12:10:17 +0200

    else Respond with serializer errors 400

--- posts/views.py
+++ posts/views.py
@@ -64,3 +64,6 @@ class PostDetail(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
