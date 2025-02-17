94e0660628b5323a953c694a845e616aeca42dc3
vikdts <v.d.tsareva@gmail.com>
2025-02-17 14:54:26 +0200

    add owner profile id ad read only

--- comments/serializers.py
+++ comments/serializers.py
@@ -4,6 +4,7 @@ from .models import Comment
 class CommentSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
 
 
     def get_is_owner(self, obj):
