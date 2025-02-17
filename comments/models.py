3cd426e8837e1517376fd0edf89c54b006f56df9
vikdts <v.d.tsareva@gmail.com>
2025-02-17 14:25:31 +0200

    add post point to Post model

--- comments/models.py
+++ comments/models.py
@@ -5,3 +5,4 @@ from posts.models import Post
 # Create your models here.
 class Comment(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
