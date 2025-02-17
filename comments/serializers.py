1f04b70800f338194bf66bd0a7ae987eaf001e49
vikdts <v.d.tsareva@gmail.com>
2025-02-17 14:46:04 +0200

    add owner as read only field

--- comments/serializers.py
+++ comments/serializers.py
@@ -2,4 +2,5 @@ from rest_framework import serializers
 from .models import Comment
 
 class CommentSerializer(serializers.ModelSerializer):
-    
+    owner = serializers.ReadOnlyField(source='owner.username')
+
