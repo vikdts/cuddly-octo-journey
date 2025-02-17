8d7504c001bfe055b0b5ebad8542ee39e8285093
vikdts <v.d.tsareva@gmail.com>
2025-02-17 17:50:28 +0200

    import CommentSerializer and CommentDetailSerializer

--- comments/views.py
+++ comments/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Comment
-
+from .serializers import CommentSerializer, CommentDetailSerializer
 
 # Create your views here.
