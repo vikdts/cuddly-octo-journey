8fea303b1b382514e387029fb0410cea40ce8e3d
vikdts <v.d.tsareva@gmail.com>
2025-02-17 14:26:13 +0200

    add created_at and updated_at timestamps

--- comments/models.py
+++ comments/models.py
@@ -6,3 +6,5 @@ from posts.models import Post
 class Comment(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
