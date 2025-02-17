4701d64cd007f32d3fad470f7600459552be83f5
vikdts <v.d.tsareva@gmail.com>
2025-02-17 14:54:51 +0200

    add profile img field

--- comments/serializers.py
+++ comments/serializers.py
@@ -5,7 +5,7 @@ class CommentSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
-
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
     def get_is_owner(self, obj):
 
