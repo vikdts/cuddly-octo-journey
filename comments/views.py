fb7755823bcfc7eed4959c656cde1ac752800b88
vikdts <v.d.tsareva@gmail.com>
2025-02-17 17:49:49 +0200

    import Comment model

--- comments/views.py
+++ comments/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics, permissions
 from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Comment
 
 
 # Create your views here.
