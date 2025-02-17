a051b1b646561ee0bf2f6bb77c5c5a6b3cf5eabb
vikdts <v.d.tsareva@gmail.com>
2025-02-17 14:26:35 +0200

    add content text field

--- comments/models.py
+++ comments/models.py
@@ -8,3 +8,4 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField()
