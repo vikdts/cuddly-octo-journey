5a24ac6b5e147617e9db03c0c55db5d586a96c3d
vikdts <v.d.tsareva@gmail.com>
2025-02-18 15:46:09 +0200

    raise ValidationError if user like the post twice

--- likes/serializers.py
+++ likes/serializers.py
@@ -12,3 +12,7 @@ class LikeSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         try:
             return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
