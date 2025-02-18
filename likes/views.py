bb14066eb8721ea103cb590fa039b6455c038ef6
vikdts <v.d.tsareva@gmail.com>
2025-02-18 15:55:23 +0200

    import Like model and LikeSerializer

--- likes/views.py
+++ likes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics, permissions
 from drf_api.permissions import IsOwnerOrReadOnly
-
+from likes.models import Like
+from likes.serializers import LikeSerializer
 
 # Create your views here.
