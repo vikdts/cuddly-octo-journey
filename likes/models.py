24454dae3b193e1e2d70c617d03c7863d5df1f05
vikdts <v.d.tsareva@gmail.com>
2025-02-18 14:45:32 +0200

    create post field link model, add related_name as likes

--- likes/models.py
+++ likes/models.py
@@ -5,3 +5,6 @@ from posts.models import Post
 # Create your models here.
 class Like(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, related_name='likes', on_delete=models.CASCADE
+    )
