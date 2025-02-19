934581696e5d8a692f218c644d0ddb1ac9adf0cb
vikdts <v.d.tsareva@gmail.com>
2025-02-19 13:58:36 +0200

    raise an error if dublicate entry

--- followers/serializers.py
+++ followers/serializers.py
@@ -15,3 +15,5 @@ class FollowerSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         try:
             return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({'detail': 'possible duplicate'})
