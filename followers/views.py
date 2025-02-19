a51377230ebd0c10f61b5b4c7ef67fb27a2f8287
vikdts <v.d.tsareva@gmail.com>
2025-02-19 14:08:33 +0200

    set queryset attribute to all followers

--- followers/views.py
+++ followers/views.py
@@ -7,3 +7,4 @@ from .serializers import FollowerSerializer
 # Create your views here.
 class FollowerList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Follower.objects.all()
