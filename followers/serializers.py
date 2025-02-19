f86d2f0a7d570e3b3173eaf614851deb1b0595b9
vikdts <v.d.tsareva@gmail.com>
2025-02-19 13:46:09 +0200

    add owner to serializer as read only

--- followers/serializers.py
+++ followers/serializers.py
@@ -2,3 +2,4 @@ from rest_framework import serializers
 from .models import Follower
 
 class FollowerSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
