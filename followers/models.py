ed5e539aca61aa2d06dd8198dc555f4d439338cf
vikdts <v.d.tsareva@gmail.com>
2025-02-19 13:35:12 +0200

    add created_at timestamp

--- followers/models.py
+++ followers/models.py
@@ -9,3 +9,4 @@ class Follower(models.Model):
     followed = models.ForeignKey(
         User, related_name='followed', on_delete=models.CASCADE
     )
+    created_at = models.DateTimeField(auto_now_add=True)
