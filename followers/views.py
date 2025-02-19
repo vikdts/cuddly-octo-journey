dcd37c75925ee7ac6c53b4440e530a8cd0530e45
vikdts <v.d.tsareva@gmail.com>
2025-02-19 14:05:08 +0200

    import Follower model

--- followers/views.py
+++ followers/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics, permissions
 from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Follower
 
 # Create your views here.
