d38e003424d33b5b98aa196252f57d2d40811711
vikdts <v.d.tsareva@gmail.com>
2025-02-19 14:05:33 +0200

    import FollowerSerializer

--- followers/views.py
+++ followers/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Follower
+from .serializers import FollowerSerializer
 
 # Create your views here.
