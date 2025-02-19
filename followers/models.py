e0ea210b616f7f4b200513c2f407e439427f2c96
vikdts <v.d.tsareva@gmail.com>
2025-02-19 13:31:36 +0200

    add owner link to User, access following by attribute

--- followers/models.py
+++ followers/models.py
@@ -3,3 +3,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Follower(models.Model):
+    owner = models.ForeignKey(
+        User, related_name='following', on_delete=models.CASCADE
+    )
