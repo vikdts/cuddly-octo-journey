f75ed8c532c84f0ee6c701becf1e521f168730ba
vikdts <v.d.tsareva@gmail.com>
2025-02-26 00:25:49 +0200

    link profile_id to owner

--- ads/serializers.py
+++ ads/serializers.py
@@ -4,3 +4,4 @@ from rest_framework import serializers
 class AdoptSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
