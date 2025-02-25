2ab4fd0299f448b7bb4feccb53fe5065295ff337
vikdts <v.d.tsareva@gmail.com>
2025-02-25 14:45:46 +0200

    add create and update timestamps

--- ads/models.py
+++ ads/models.py
@@ -13,5 +13,5 @@ class Ad(models.Model):
     ('walden', 'Walden'), ('xpro2', 'X-pro II')
     ]
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
