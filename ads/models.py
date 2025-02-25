228eaccca55e14a7df6225e940c4cbfd72a1c077
vikdts <v.d.tsareva@gmail.com>
2025-02-25 14:44:20 +0200

    add filter choices

--- ads/models.py
+++ ads/models.py
@@ -3,3 +3,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Ad(models.Model):
+    image_filter_choices = [
+    ('_1977', '1977'), ('brannan', 'Brannan'),
+    ('earlybird', 'Earlybird'), ('hudson', 'Hudson'),
+    ('inkwell', 'Inkwell'), ('lofi', 'Lo-Fi'),
+    ('kelvin', 'Kelvin'), ('normal', 'Normal'),
+    ('nashville', 'Nashville'), ('rise', 'Rise'),
+    ('toaster', 'Toaster'), ('valencia', 'Valencia'),
+    ('walden', 'Walden'), ('xpro2', 'X-pro II')
+    ]
