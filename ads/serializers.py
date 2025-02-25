713e39cc48e27e9805e14f45d02125206b65bff7
vikdts <v.d.tsareva@gmail.com>
2025-02-26 00:21:19 +0200

    specify owner as a ReadOnlyField

--- ads/serializers.py
+++ ads/serializers.py
@@ -2,4 +2,4 @@ from .models import Ad
 from rest_framework import serializers
 
 class AdoptSerializer(serializers.ModelSerializer):
-    
+    owner = serializers.ReadOnlyField(source='owner.username')
