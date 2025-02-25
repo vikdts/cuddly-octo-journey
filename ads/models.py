5bbc1a20c0b34ad0a922938319d2309722ac3f82
vikdts <v.d.tsareva@gmail.com>
2025-02-25 14:46:28 +0200

    add title field

--- ads/models.py
+++ ads/models.py
@@ -15,3 +15,4 @@ class Ad(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
