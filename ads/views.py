b070d484a1a729aa62a43a39c27e539380457089
vikdts <v.d.tsareva@gmail.com>
2025-02-26 00:50:47 +0200

    import AdSerializer and custom permission to views

--- ads/views.py
+++ ads/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import Ad
+from .serializers import AdSerializer
+from drf_api.permissions import IsOwnerOrReadOnly
 
 # Create your views here.
