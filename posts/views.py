a610d64ddadf0ad378d3051697ffceb6ba33aaee
vikdts <v.d.tsareva@gmail.com>
2025-02-25 14:23:44 +0200

    update PostDetail

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,5 @@
-from django.http import Http404
 from django.shortcuts import render
 from rest_framework import generics, permissions
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from .models import Post
 from .serializers import PostSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
@@ -21,41 +18,7 @@ class PostList(generics.ListCreateAPIView):
 
     
 
-class PostDetail(APIView):
-
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = PostSerializer
-
-    def get_object(self, pk):
-        try:
-            post = Post.objects.get(pk=pk)
-            self.check_object_permissions(self.request, post)
-            return post
-        except Post.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        post = self.get_object(pk)
-        serializer = PostSerializer(
-            post, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        post = self.get_object(pk)
-        serializer = PostSerializer(
-            post, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
-
-    def delete(self, request, pk):
-        post = self.get_object(pk)
-        post.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    queryset = Post.objects.all()
