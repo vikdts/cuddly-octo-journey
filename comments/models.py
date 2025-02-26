239e5728da973dd51a02160dfa56ee36c5771b12
vikdts <v.d.tsareva@gmail.com>
2025-02-26 14:19:28 +0200

    replace post field with content_type

--- comments/models.py
+++ comments/models.py
@@ -7,7 +7,7 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 # Create your models here.
 class Comment(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField()
