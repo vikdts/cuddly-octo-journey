7e07d951ab3ea0aeba6073842585e2bfa2a7cf70
vikdts <v.d.tsareva@gmail.com>
2025-02-26 14:47:33 +0200

    set defaults to null

--- comments/models.py
+++ comments/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
-from posts.models import Post, Ad
+from posts.models import Post
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
 class Comment(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, default=ContentType.objects.get_for_model(Post))
-    object_id = models.PositiveIntegerField(default=Post.objects.first().id)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True)
+    object_id = models.PositiveIntegerField(null=True)
     content_object = GenericForeignKey('content_type', 'object_id')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
