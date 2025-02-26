3af3797749910a15abcb5de81404aaa2324d4b04
vikdts <v.d.tsareva@gmail.com>
2025-02-26 14:22:03 +0200

    add GenericForeignKey to content_object and link content_type and object_id

--- comments/models.py
+++ comments/models.py
@@ -9,6 +9,7 @@ class Comment(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField()
