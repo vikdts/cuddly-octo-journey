e71ada4cb7fcac50e38d2341de83af9a3db90d21
vikdts <v.d.tsareva@gmail.com>
2025-02-27 00:53:15 +0200

    rv post field from Meta class and add content_type and object_id

--- comments/serializers.py
+++ comments/serializers.py
@@ -17,8 +17,8 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = [
-            'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
-            'post', 'created_at', 'updated_at', 'content'
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image', 
+            'content_type', 'object_id', 'created_at', 'updated_at', 'content'
         ]
 
 class CommentDetailSerializer(CommentSerializer):
