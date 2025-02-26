2e655f8c8c2972a0ee87b31a1047781b0e2fa187
vikdts <v.d.tsareva@gmail.com>
2025-02-26 14:16:21 +0200

    import ContentType to Comment model

--- comments/models.py
+++ comments/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from posts.models import Post
+from django.contrib.contenttypes.models import ContentType
 
 # Create your models here.
 class Comment(models.Model):
