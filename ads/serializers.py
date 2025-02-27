6856cb6b97f0cbb20bbb5d301b0b5ab743c9333b
vikdts <v.d.tsareva@gmail.com>
2025-02-27 13:40:11 +0200

    update AdSerializer

--- ads/serializers.py
+++ ads/serializers.py
@@ -1,11 +1,15 @@
 from .models import Ad
 from rest_framework import serializers
+from comments.serializers import CommentSerializer
+from likes.serializers import LikeSerializer
 
 class AdSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    comments = CommentSerializer(many=True, read_only=True)
+    likes = LikeSerializer(many=True, read_only=True)
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -29,5 +33,6 @@ class AdSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
             'created_at', 'updated_at', 'title',
-            'content', 'image', 'is_active','map_embed_code'
+            'content', 'image', 'is_active','map_embed_code', 
+            'comments', 'likes'
         ]
