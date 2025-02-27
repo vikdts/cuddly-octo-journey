3d0cba601021b55e67a38a84017ff94900d00e2e
vikdts <v.d.tsareva@gmail.com>
2025-02-27 13:42:57 +0200

    update LikeSerializer

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,18 +1,19 @@
 from rest_framework import serializers
 from likes.models import Like
 from django.db import IntegrityError
-from django.contrib.contenttypes.models import ContentType
 
 class LikeSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    content_type = serializers.SlugRelatedField(slug_field="model", queryset=ContentType.objects.all())
     object_id = serializers.IntegerField()
 
     class Meta:
         model = Like
-        fields = ['id', 'owner', 'created_at', 'content_type', 'object_id']
+        fields = ['id', 'owner', 'created_at', 'object_id', 'content_object']
 
     def create(self, validated_data):
-        if Like.objects.filter(owner=validated_data['owner'], content_type=validated_data['content_type'], object_id=validated_data['object_id']).exists():
-            raise serializers.ValidationError({'detail': 'possible duplicate'})
-        return super().create(validated_data)
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
