61d8796a781433527ce7f260fcb107c4409b850c
vikdts <v.d.tsareva@gmail.com>
2025-03-01 17:14:12 +0200

    typo included fields

--- posts/serializers.py
+++ posts/serializers.py
@@ -40,5 +40,5 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image', 'image_filter', 'id'
+            'title', 'content', 'image', 'image_filter', 'like_id'
         ]
