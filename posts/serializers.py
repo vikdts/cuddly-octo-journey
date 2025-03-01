7f748850d75ff8c418a40b2e2755a5fa57ad9d4b
vikdts <v.d.tsareva@gmail.com>
2025-03-01 17:09:56 +0200

    retrieve like_id if user  is aauthenticated or None

--- posts/serializers.py
+++ posts/serializers.py
@@ -28,7 +28,12 @@ class PostSerializer(serializers.ModelSerializer):
 
     def get_like_id(self, obj):
         user = self.context['request'].user
-
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return like.id if like else None
+        return None
 
     class Meta:
         model = Post
