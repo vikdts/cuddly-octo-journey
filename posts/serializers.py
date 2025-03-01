715101d33f22a476ab16925c3e59b9c16a72996c
vikdts <v.d.tsareva@gmail.com>
2025-03-02 01:07:01 +0200

    add likes and comment count to fields list

--- posts/serializers.py
+++ posts/serializers.py
@@ -42,5 +42,6 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image', 'image_filter', 'like_id'
+            'title', 'content', 'image', 'image_filter', 'like_id',
+            'likes_count', 'comments_count',
         ]
