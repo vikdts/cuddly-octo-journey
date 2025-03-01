c7fd851eef913a32947a9564c8e36cfbf9386a13
vikdts <v.d.tsareva@gmail.com>
2025-03-01 17:15:27 +0200

    include like_id to ads serialiers fields

--- ads/serializers.py
+++ ads/serializers.py
@@ -1,11 +1,13 @@
 from .models import Ad
 from rest_framework import serializers
+from likes.models import Like
 
 class AdSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -29,5 +31,5 @@ class AdSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
             'created_at', 'updated_at', 'title',
-            'content', 'image', 'is_active','map_embed_code', 
+            'content', 'image', 'is_active','map_embed_code', 'like_id'
         ]
