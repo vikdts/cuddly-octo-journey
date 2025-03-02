1d43b874e7f73ee02c284d556463027fd4ca7eda
vikdts <v.d.tsareva@gmail.com>
2025-03-02 03:11:27 +0200

    extend UserDetailsSerializer to current user, add profile id and img

--- drf_api/serializers.py
+++ drf_api/serializers.py
@@ -1,2 +1,6 @@
 from dj_rest_auth.serializers import UserDetailsSerializer
 from rest_framework import serializers
+
+class CurrentUserSerializer(UserDetailsSerializer):
+    profile_id = serializers.ReadOnlyField(source='profile.id')
+    profile_image = serializers.ReadOnlyField(source='profile.image.url')
