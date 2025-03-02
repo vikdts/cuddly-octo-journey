e5c6d58b3836f0c8239edb184dd7a189e5905360
vikdts <v.d.tsareva@gmail.com>
2025-03-02 15:59:40 +0200

    if dev is not prese nt set to json renderer

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -36,6 +36,10 @@ REST_FRAMEWORK = {
         'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
