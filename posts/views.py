3b282e63529214c3560b091bd765d7dc584158a4
vikdts <v.d.tsareva@gmail.com>
2025-03-02 02:04:59 +0200

    set model fields filter

--- posts/views.py
+++ posts/views.py
@@ -21,6 +21,11 @@ class PostList(generics.ListCreateAPIView):
         filters.SearchFilter,
         DjangoFilterBackend
     ]
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'likes__owner__profile',
+        'owner__profile',
+    ]
     search_fields = [
         'owner__username',
         'title',
