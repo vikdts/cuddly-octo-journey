1e2dfbce2a9ddcaab5fc17606f4c929df920ad75
vikdts <v.d.tsareva@gmail.com>
2025-03-02 21:29:07 +0200

    def logout view, pass acess and refresh secure

--- drf_api/views.py
+++ drf_api/views.py
@@ -9,3 +9,26 @@ def root_route(request):
     return Response({
         "message": "Welcome !"
     })
+
+@api_view(['POST'])
+def logout_route(request):
+    response = Response()
+    response.set_cookie(
+        key=JWT_AUTH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    response.set_cookie(
+        key=JWT_AUTH_REFRESH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    return response
