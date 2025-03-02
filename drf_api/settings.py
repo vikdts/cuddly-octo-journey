b565ec7030553af10d2e963bb1d1c19f780fdb20
vikdts <v.d.tsareva@gmail.com>
2025-03-02 03:07:05 +0200

    enable token authentification

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -34,6 +34,8 @@ REST_FRAMEWORK = {
     )]
 }
 
+REST_USE_JWT = True
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
