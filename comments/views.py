4c8926c3a8d9b8836769a03dd7f7bd9d4b9c6430
vikdts <v.d.tsareva@gmail.com>
2025-03-02 02:34:04 +0200

    import DjangoFilterBackend to comments views

--- comments/views.py
+++ comments/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
