3a0808374277a34953219cebfaee78f7bb821499
vikdts <v.d.tsareva@gmail.com>
2025-03-02 16:39:19 +0200

    set cors origins

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -105,6 +105,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    origin for origin in [
+        os.environ.get('CLIENT_ORIGIN'),
+        os.environ.get('CLIENT_ORIGIN_DEV')
+    ] if origin
+]
+
 ROOT_URLCONF = 'drf_api.urls'
 
 TEMPLATES = [
