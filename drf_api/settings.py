815897c1abf458fa915c9bd34315573168aff83f
vikdts <v.d.tsareva@gmail.com>
2025-03-02 23:47:21 +0200

    deployment check

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -109,12 +109,6 @@ MIDDLEWARE = [
 CLIENT_ORIGIN = os.getenv('CLIENT_ORIGIN')
 CLIENT_ORIGIN_DEV = os.getenv('CLIENT_ORIGIN_DEV')
 
-CORS_ALLOWED_ORIGINS = [
-    origin for origin in [
-        os.environ.get('CLIENT_ORIGIN'),
-        os.environ.get('CLIENT_ORIGIN_DEV')
-    ] if origin
-]
 
 CORS_ALLOW_CREDENTIALS = True
 
